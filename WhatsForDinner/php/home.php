d2deeb69eadb57ff99f5ab59590034714f073d4e
Angie Chen <a_chen2@uncg.edu>
2023-04-04 12:14:13 -0400

    4/4/23 Home Page now includes link to user profile if logged in

--- WhatsForDinner/php/home.php
+++ WhatsForDinner/php/home.php
@@ -1,4 +1,10 @@
 <h1>What's Dinner Tonight?</h1>
+<?php
+session_start();
+// required 
+require "connection.php";
+require "common.php"; ?>
+
 <style>
 a:link, a:visited {
   color: #000000;
@@ -9,6 +15,11 @@ a:hover, a:active {
   text-decoration: none
 }
 </style>
+
+<?php if ($_SESSION['loggedin'] = TRUE) { ?>     
+  <li><a href="userProfile.php"><strong>Profile</strong></a></li>
+<?php } ?>
+
 <li><a href="recipeMultiSearch.php"><strong>Search by Ingredients</strong></a></li>
 <li><a href="recipeMatchcaseSearch.php"><strong>Search by Recipe Name</strong></a></li> 
 <li><strong>Search by Tags </strong><a href="entrees.php"><strong>[ Entree ]</strong></a> <a href="sides.php"><strong>[ Side ]</strong></a> <a href="desserts.php"><strong>[ Dessert ]</strong></a></li> 
